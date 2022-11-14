5503ab87705dca9f4f9c21adaed5a783d43ff3bd
parachvte <ryannx6@gmail.com>
2022-11-14 16:05:05 +0800

    Fix: Fix socket issues on Windows (#16)

--- network/SnapmakerOutputDevicePlugin.py
+++ network/SnapmakerOutputDevicePlugin.py
@@ -1,15 +1,105 @@
-from PyQt6.QtCore import QTimer
-from PyQt6.QtNetwork import QNetworkInterface, QUdpSocket, QAbstractSocket
+import socket
 
+from PyQt6.QtCore import QTimer
+from PyQt6.QtNetwork import QNetworkInterface, QUdpSocket, QAbstractSocket, QHostAddress, QNetworkAddressEntry
 from UM.Application import Application
 from UM.Logger import Logger
 from UM.OutputDevice.OutputDevicePlugin import OutputDevicePlugin
 from UM.Platform import Platform
+from UM.Signal import signalemitter, Signal
 
 from .SnapmakerJ1OutputDevice import SnapmakerJ1OutputDevice
 from ..config import MACHINE_NAME
 
 DISCOVER_PORT = 20054
+
+
+@signalemitter
+class DiscoverSocket:
+    dataReady = Signal()
+
+    def __init__(self, address_entry: QNetworkAddressEntry) -> None:
+        self._address_entry = address_entry
+        self._broadcast_address = address_entry.broadcast()
+
+        self._socket = None  # internal socket
+
+        self._collect_timer = QTimer()
+        self._collect_timer.setInterval(200)
+        self._collect_timer.setSingleShot(True)
+        self._collect_timer.timeout.connect(self.__collect)
+
+    @property
+    def address(self) -> QHostAddress:
+        return self._address_entry.ip()
+
+    def bind(self) -> bool:
+        sock = QUdpSocket()
+
+        bind_result = sock.bind(self._address_entry.ip(), mode=QAbstractSocket.BindFlag.DontShareAddress |
+                                                               QAbstractSocket.BindFlag.ReuseAddressHint)
+        if not bind_result:
+            return False
+
+        if Platform.isWindows():
+            # On Windows, QUdpSocket is unable to receive broadcast data, we use original socket instead
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            sock.settimeout(0.2)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+            self._socket = sock
+        else:
+            # On Unix, we use socket interface provided by Qt 6
+            self._socket = sock
+            sock.readyRead.connect(self.__read)
+
+        return True
+
+    def discover(self, message: bytes) -> None:
+        if isinstance(self._socket, QUdpSocket):
+            self._socket.writeDatagram(message, self._broadcast_address, DISCOVER_PORT)
+        else:
+            self._socket.sendto(message, (self._broadcast_address.toString(), DISCOVER_PORT))
+            self._collect_timer.start()
+
+    def abort(self) -> None:
+        if not self._socket:
+            return
+
+        if isinstance(self._socket, QUdpSocket):
+            self._socket.abort()
+        else:
+            self._socket.close()
+
+        self._socket = None
+
+    def __read(self) -> None:
+        while self._socket.hasPendingDatagrams():
+            data = self._socket.receiveDatagram()
+            if data.isValid() and not data.senderAddress().isNull():
+                try:
+                    message = bytes(data.data()).decode("utf-8")
+                    self.dataReady.emit(message)
+                except UnicodeDecodeError:
+                    pass
+
+    def __collect(self) -> None:
+        if isinstance(self._socket, QUdpSocket):
+            return
+
+        while True:
+            try:
+                msg, addr = self._socket.recvfrom(128)
+            except TimeoutError:
+                break
+
+            ip, _ = addr
+            try:
+                message = msg.decode("utf-8")
+                self.dataReady.emit(message)
+            except UnicodeDecodeError:
+                pass
 
 
 class SnapmakerOutputDevicePlugin(OutputDevicePlugin):
@@ -30,7 +120,6 @@ class SnapmakerOutputDevicePlugin(OutputDevicePlugin):
 
     def __prepare(self) -> None:
         self._discover_sockets = []
-        available_port = 20054
         for interface in QNetworkInterface.allInterfaces():
             for address_entry in interface.addressEntries():
                 address = address_entry.ip()
@@ -39,25 +128,23 @@ class SnapmakerOutputDevicePlugin(OutputDevicePlugin):
                 if address.protocol() != QAbstractSocket.NetworkLayerProtocol.IPv4Protocol:
                     continue
 
-                Logger.info("Discovering printers on network interface: %s", address.toString())
-                socket = QUdpSocket()
-                if Platform.isWindows():
-                    # Need to bind to a specified port in order to let QUdpSocket receive datagram
-                    socket.bind(address, available_port)
-                    available_port += 1
-                else:
-                    socket.bind(address)
-                socket.readyRead.connect(lambda: self._readSocket(socket))
-                self._discover_sockets.append((socket, address_entry))
+                sock = DiscoverSocket(address_entry)
+                if sock.bind():
+                    Logger.info("Discovering printers on network interface: %s", address.toString())
+                    sock.dataReady.connect(self.__onData)
+                    self._discover_sockets.append(sock)
 
     def __discover(self) -> None:
         if not self._discover_sockets:
             self.__prepare()
 
-        for socket, address_entry in self._discover_sockets:
-            socket.writeDatagram(b"discover", address_entry.broadcast(), DISCOVER_PORT)
+        for sock in self._discover_sockets:
+            Logger.info("Discovering networked printer... (interface: %s)", sock.address.toString())
+            sock.discover(b"discover")
 
-    def __parseMessage(self, ip: str, msg: str) -> None:
+        # TODO: remove output devices that not reply message for a period of time
+
+    def __onData(self, msg: str) -> None:
         """Parse message.
 
         e.g. Snapmaker J1@172.18.0.2|model:J1|status:IDLE
@@ -85,22 +172,17 @@ class SnapmakerOutputDevicePlugin(OutputDevicePlugin):
 
         device = self.getOutputDeviceManager().getOutputDevice(device_id)
         if not device:
-            Logger.info("Discovered Snapmaker J1: %s@%s", name, address)
+            Logger.info("Discovered Snapmaker J1 printer: %s@%s", name, address)
             device = SnapmakerJ1OutputDevice(device_id, address, properties)
             self.getOutputDeviceManager().addOutputDevice(device)
 
-    def _readSocket(self, socket: QUdpSocket) -> None:
-        while socket.hasPendingDatagrams():
-            data = socket.receiveDatagram()
-            if data.isValid() and not data.senderAddress().isNull():
-                ip = data.senderAddress().toString()
-                try:
-                    msg = bytes(data.data()).decode("utf-8")
-                    self.__parseMessage(ip, msg)
-                except UnicodeDecodeError:
-                    pass
-
     def start(self) -> None:
+        # check for current global container
+        global_stack = Application.getInstance().getGlobalContainerStack()
+        machine_name = global_stack.getProperty("machine_name", "value")
+        if machine_name != MACHINE_NAME:
+            return
+
         if not self._discover_timer.isActive():
             self._discover_timer.start()
             Logger.info("Snapmaker J1 discovering started.")
@@ -109,8 +191,8 @@ class SnapmakerOutputDevicePlugin(OutputDevicePlugin):
         if self._discover_timer.isActive():
             self._discover_timer.stop()
 
-        for socket in self._discover_sockets:
-            socket.abort()
+        for sock in self._discover_sockets:
+            sock.abort()
 
         Logger.info("Snapmaker J1 discovering stopped.")
 
